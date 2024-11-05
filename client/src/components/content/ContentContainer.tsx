8bc4085f356e60157dc535c3da226a2eff5eef5c
gary-rivera <a.gary.rivera@gmail.com>
2024-11-05 14:57:42 -0800

    animation styling of ContentContainer

--- client/src/components/content/ContentContainer.tsx
+++ client/src/components/content/ContentContainer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import {
 	Box,
 	Flex,
@@ -9,11 +9,27 @@ import {
 	Text,
 	VStack,
 } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 import { MdCircle as FilledCircle } from 'react-icons/md';
 import { FaCircleHalfStroke as HalfFilledCircle } from 'react-icons/fa6';
 import { FaCircle as EmptyCircle } from 'react-icons/fa';
 
 function ContentContainer() {
+	const [activeTab, setActiveTab] = useState('experience');
+	const [height, setHeight] = useState('auto');
+	const contentRef = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		if (contentRef.current) {
+			const scrollHeight = `${contentRef.current.scrollHeight}px`;
+			setHeight(scrollHeight);
+		}
+	}, [activeTab]);
+
+	const toggleActiveTab = () => {
+		setActiveTab(activeTab === 'experience' ? 'projects' : 'experience');
+	};
+
 	// DONE: timeline to the left
 	// DONE: Tab toggle + Container
 	// TODO: experience tab
@@ -26,12 +42,7 @@ function ContentContainer() {
 	const Timeline = ({ year }: { year: string }) => (
 		<Flex alignItems="start" direction="column" h="100%" py={4} pl={2}>
 			<VStack position="relative" h="inherit">
-				<Flex
-					position="relative"
-					h="95%"
-					justifyContent="center"
-					// flexDirection="column"
-				>
+				<Flex position="relative" h="95%" justifyContent="center">
 					<FilledCircle size="8px" />
 					<Box
 						position="absolute"
@@ -58,19 +69,19 @@ function ContentContainer() {
 		<Grid
 			className="content-container"
 			gridTemplateRows={'40px repeat(1, 1fr)'}
-			gridTemplateColumns="repeat(5, 1fr)"
+			gridTemplateColumns="repeat(6, 1fr)"
 			mt="2rem"
 		>
-			<GridItem rowSpan={1} colSpan={1} colStart={1} rowStart={2} bg="tomato">
-				<Timeline year="2024" />
-				{/* <HalfFilledCircle /> */}
-				{/* <EmptyCircle /> */}
-			</GridItem>
-			<GridItem rowSpan={2} colSpan={4}>
+			<GridItem
+				className="nav-tabs-grid-item"
+				rowSpan={1}
+				colSpan={2}
+				colStart={2}
+			>
 				<Tabs.Root
 					variant="line"
 					defaultValue="experience"
-					/*value={value} onValueChange={(e) => setValue(e.value)}*/
+					onValueChange={toggleActiveTab}
 				>
 					<Tabs.List rounded="l3" px="0">
 						<Tabs.Trigger value="experience" bg="none" bgClip="inherit">
@@ -85,24 +96,66 @@ function ContentContainer() {
 							outlineColor="yellow"
 						/>
 					</Tabs.List>
-
-					<Tabs.Content bg="green.300" value="experience" py={2}>
-						<ul>
-							<li>Item 1</li>
-							<li>Item 2</li>
-							<li>Item 3</li>
-							<li>Item 4</li>
-							<li>Item 5</li>
-							<li>Item 6</li>
-							<li>Item 7</li>
-							<li>Item 8</li>
-							<li>Item 9</li>
-						</ul>
-					</Tabs.Content>
-					<Tabs.Content bg="green.300" value="projects">
-						Projects panel
-					</Tabs.Content>
 				</Tabs.Root>
+			</GridItem>
+			<GridItem
+				className="timeline-grid-item"
+				rowSpan={1}
+				colSpan={1}
+				rowStart={2}
+				bg="tomato"
+			>
+				<Flex alignItems="start" direction="column" h="100%" py={4} pl={2}>
+					<VStack position="relative" h="inherit">
+						<Flex position="relative" h="95%" justifyContent="center">
+							<FilledCircle size="8px" />
+							<Box
+								position="absolute"
+								flexGrow={1}
+								top={2.5}
+								w={0}
+								h="100%"
+								className="tl-progress-bar"
+								borderLeft="1px dashed black"
+							/>
+						</Flex>
+
+						<HStack position="absolute" left="3" top="-2">
+							<Text>2024</Text>
+						</HStack>
+						<HStack></HStack>
+					</VStack>
+				</Flex>
+			</GridItem>
+			<GridItem className="content-grid-item" rowSpan={1} colSpan={5}>
+				{/* Motion wrapper for animating height */}
+				<motion.div
+					key={activeTab}
+					ref={contentRef}
+					initial={{ opacity: 0, height: 0 }}
+					animate={{ opacity: 1, height }}
+					exit={{ opacity: 0, height: 0 }}
+					transition={{ duration: 0.6 }}
+					style={{ overflow: 'hidden' }}
+				>
+					{activeTab === 'experience' && (
+						<Box bg="green.300" p={4}>
+							<ul>
+								<li>Item 1</li>
+								<li>Item 2</li>
+								<li>Item 3</li>
+								<li>Item 4</li>
+								<li>Item 5</li>
+							</ul>
+						</Box>
+					)}
+
+					{activeTab === 'projects' && (
+						<Box bg="green.300" p={4}>
+							Projects panel content
+						</Box>
+					)}
+				</motion.div>
 			</GridItem>
 		</Grid>
 	);

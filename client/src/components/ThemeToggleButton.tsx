8d62a75526df932816407d1d77731d7d8fa28a78
gary-rivera <a.gary.rivera@gmail.com>
2024-11-06 19:48:15 -0800

    deprecate CSS modules - ThemeToggleButton.tsx

--- client/src/components/ThemeToggleButton.tsx
+++ client/src/components/ThemeToggleButton.tsx
@@ -1,12 +1,15 @@
 import { useThemeContext } from '../context/ThemeContext';
-import styles from '../styles/ThemeToggleButton.module.css';
 import { Button } from '@chakra-ui/react';
 
 const ThemeToggleButton = () => {
 	const { theme, toggleTheme } = useThemeContext();
 
 	return (
-		<Button onClick={toggleTheme} className={styles.button}>
+		<Button
+			onClick={toggleTheme}
+			bg="var(--secondary-bg-color)"
+			color="var(--text-color)"
+		>
 			Switch to {theme === 'light' ? 'dark' : 'light'} mode
 		</Button>
 	);

68a39709ef16c2ae95b634c572a86a56ec29b459
gary-rivera <a.gary.rivera@gmail.com>
2024-11-06 19:48:01 -0800

    deprecate CSS module - MainHeader.tsx

--- client/src/components/header/MainHeader.tsx
+++ client/src/components/header/MainHeader.tsx
@@ -1,10 +1,9 @@
 import TypingEffectHeader from './TypingEffectHeader';
-import styles from '../../styles/MainHeader.module.css';
 import { Box, HStack } from '@chakra-ui/react';
 
 function MainHeader() {
 	return (
-		<Box className={styles.header}>
+		<Box>
 			<TypingEffectHeader />
 			<HStack>just some dweeb pretending to know what they're doing.</HStack>
 		</Box>

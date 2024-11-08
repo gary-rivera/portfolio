2ad21c6d0c036882bddad575eb9d59138a99e7a2
gary-rivera <a.gary.rivera@gmail.com>
2024-11-08 15:27:36 -0800

    configure gh octo gql api fetcher

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,19 +1,80 @@
-import { Octokit } from '@octokit/rest'
 import { graphql } from '@octokit/graphql'
+import type { GraphQlQueryResponseData } from "@octokit/graphql";
 
 const VITE_GH_API_TOKEN = import.meta.env.VITE_GH_API_TOKEN
+const GH_USERNAME = "gary-rivera"
 
-export const octokitFetcher: Octokit = new Octokit({
-  auth: VITE_GH_API_TOKEN,
-  mediaType: {
-    previews: ['inertia'],
-  },
-})
-export const octokitGQL = graphql.defaults({
+const GitHubGQLConfig = graphql.defaults({
   headers: {
     authorization: `token ${VITE_GH_API_TOKEN}`,
   },
 })
+
+export const fetchRepositoryData = async (username: string, repoName: string) => {
+  const query = `
+    query ($username: String!, $repoName: String!) {
+      repository(owner: $username, name: $repoName) {
+        name
+        description
+        stargazerCount
+        createdAt
+        languages(first: 5) {
+          edges {
+            node {
+              name
+            }
+          }
+        }
+        owner {
+          login
+          avatarUrl
+        }
+        primaryLanguage {
+          name
+        }
+      }
+    }
+  `
+
+  const variables = { username, repoName }
+  const { repository }: GraphQlQueryResponseData = await GitHubGQLConfig(query, variables)
+  return repository
+}
+export const fetchRepositoriesData = async (repoNames: string[]) => {
+  const queries = repoNames.map((repoName) => `
+    ${repoName}: repository(owner: "${GH_USERNAME}", name: "${repoName}") {
+      name
+      description
+      createdAt
+      url
+      languages(first: 5) {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+      primaryLanguage {
+        name
+      }
+      defaultBranchRef {
+        target {
+          ... on Commit {
+            history {
+              totalCount
+            }
+          }
+        }
+      }
+    }
+  `).join("\n")
+
+  const query = `query { ${queries} }`
+
+  const data: GraphQlQueryResponseData = await GitHubGQLConfig(query)
+  return data
+}
+// url, createdAt, total commits for repo, total commits for user (maybe more?)
 
 export const fetcher = (url: string) =>
   fetch(url, {

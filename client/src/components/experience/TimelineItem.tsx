5da2a6252c0ed26988164b21f9999629f3851860
gary-rivera <a.gary.rivera@gmail.com>
2024-11-11 18:54:51 -0800

    TimelineItem styling changes
    NOTE: this sorta undoes the overlap UI that I really liked. explore how to readd that but without doing so in a hacky way

--- client/src/components/experience/TimelineItem.tsx
+++ client/src/components/experience/TimelineItem.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Flex, Card, Em, Box, Icon, GridItem, Grid } from "@chakra-ui/react";
+import { Flex, Card, Em, HStack, Heading, Box, Icon, GridItem, Grid, useRecipe, Text } from "@chakra-ui/react";
 import { LuTarget } from "react-icons/lu";
-import { Event, events } from "@/data/experience";
+import { CareerEvent, events } from "@/data/experience";
 
 //TODO: move to MeasureDistance.ts
 type Distance = {
@@ -11,7 +11,7 @@ type Distance = {
 };
 
 type TimelineItemProps = {
-	event: Event;
+	event: CareerEvent;
 	index: number;
 	alternate: boolean;
 	iconRef: React.RefObject<HTMLDivElement>;
@@ -22,18 +22,34 @@ type TimelineItemProps = {
 	} | null)[];
 };
 
-const TimelineEventCard: React.FC<{ description: string }> = ({ description }) => (
-	<Card.Root w="auto" bg="rgba(0, 0, 0, 0.035)" h="100%" borderColor="rgba(12,92, 114, 0.2)">
-		<Card.Body>
-			<Card.Title>Event Title</Card.Title>
-			<Card.Description>{description}</Card.Description>
-		</Card.Body>
-	</Card.Root>
-);
-
+type ImpactEventCardProps = {
+	event: CareerEvent;
+};
+// TODO: just pass the whole damn object instead.
+// TODO: bind the alternating card layout to a state?
+// TODO: card title doesn't center if its only one present
+// TODO: add hover tooltip to events where employer is listed giving a quick blurb about what service they offered +/- badges
+// TODO: fix the elevated LCP (850ms) from description block
+const TimelineEventCard: React.FC<ImpactEventCardProps> = ({ event }) => {
+	const { title, description, date, origin, attributes, category } = event;
+	const recipe = useRecipe({ key: "eventCard" });
+	const styles = recipe({ category });
+	console.log({ styles });
+	return (
+		<Flex css={styles} direction="column" justifyContent="center" py="3" px="5">
+			<HStack>
+				<Heading size="md">{title}</Heading>
+				<Text fontSize="xs">{category}</Text>
+			</HStack>
+			<HStack>
+				<Text fontSize="xs">{description}</Text>
+			</HStack>
+		</Flex>
+	);
+};
 const TimelineEventDate: React.FC<{ date: string }> = ({ date }) => (
 	<Box width="fit-content" color="black" w="auto">
-		<Em width="fit-content" color="gray.600">
+		<Em fontSize="2xs" width="fit-content" color="gray.600">
 			{date}
 		</Em>
 	</Box>
@@ -51,9 +67,9 @@ const TimelinePath: React.FC<{
 		<Box
 			position="absolute"
 			top="100%"
-			width="3px"
+			width="1.5px"
 			height={`${distanceToNextTimelineIcon - 20}px`}
-			backgroundColor="orange.400"
+			bg="blackAlpha.500"
 			overflow="hidden"
 			zIndex={0}
 		/>
@@ -68,7 +84,7 @@ const TimelinePath: React.FC<{
 			justifyContent="center"
 			ref={iconRef}
 		>
-			<Icon fontSize="20px" zIndex={1} color="rgba(8, 145, 178)">
+			<Icon fontSize="20px" zIndex={1} color="#0891b2">
 				{/* <VscCircleLargeFilled opacity="0.8" /> */}
 				<LuTarget opacity="0.8" />
 			</Icon>
@@ -87,15 +103,15 @@ function TimelineItem({ event, index, alternate, iconRef, distances }: TimelineI
 				<TimelinePath iconRef={iconRef} index={index} distances={distances} />
 			</GridItem>
 			<GridItem h="115%">
-				<TimelineEventCard description={event.description} />
+				<TimelineEventCard event={event} />
 			</GridItem>
 		</>
 	);
 
 	const EventDateRightAlignedLayout = (
 		<>
-			<GridItem h="115%">
-				<TimelineEventCard description={event.description} />
+			<GridItem h="110%">
+				<TimelineEventCard event={event} />
 			</GridItem>
 			<GridItem placeSelf="center">
 				<TimelinePath iconRef={iconRef} index={index} distances={distances} />
@@ -108,7 +124,7 @@ function TimelineItem({ event, index, alternate, iconRef, distances }: TimelineI
 
 	return (
 		<Grid
-			gap={3}
+			gap={2}
 			position="relative"
 			templateColumns="1fr auto 1fr"
 			templateRows="1fr"
@@ -116,7 +132,10 @@ function TimelineItem({ event, index, alternate, iconRef, distances }: TimelineI
 			gridAutoColumns="auto"
 			alignItems="center"
 			justifyContent="center"
-			w="100%"
+			py="2"
+			// w="100%"
+			h="110%"
+			my="-1.5"
 		>
 			{alternate ? EventDateRightAlignedLayout : EventDateLeftAlignedLayout}
 		</Grid>

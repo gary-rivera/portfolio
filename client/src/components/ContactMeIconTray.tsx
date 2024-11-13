4e8559b89c60f722f6038438294536556a9d87f1
gary-rivera <a.gary.rivera@gmail.com>
2024-11-13 00:15:49 -0800

    gh + linkedin icon rendering

--- client/src/components/ContactMeIconTray.tsx
+++ client/src/components/ContactMeIconTray.tsx
@@ -1,12 +1,62 @@
 import { chakra, VStack, HStack, Flex, Box } from "@chakra-ui/react";
 import { ColorModeButton } from "@/components/ui/color-mode";
+import LinkIconFactory from "@/components/ui/chakra-fixes/LinkIconFactory";
 import LinkedInSvgIcon from "@/assets/linkedin-small.svg?react";
+import GithubSvgIcon from "@/assets/online-li-conversion.svg?react";
+import { DiGithubFull } from "react-icons/di";
+
+const LinkedInLogoSvg = () => (
+	<chakra.svg>
+		<></>
+	</chakra.svg>
+);
 
 function ContactMeIconTray() {
+	// const LinkedInLogo = chakra(LinkedInLogoSvg);
+
 	return (
-		<VStack>
-			<ColorModeButton w="2rem" />
-		</VStack>
+		<HStack h="2.5rem">
+			<ColorModeButton w="1rem" />
+			<chakra.button
+				border="2px solid red"
+				color="blackAlpha.800"
+				py="0.5"
+				px="1"
+				h="inherit"
+				_hover={{ bg: "blackAlpha.200", color: "blackAlpha.950" }}
+			>
+				<chakra.a>
+					<LinkedInSvgIcon
+						fill="currentColor"
+						style={{
+							width: "auto",
+							height: "inherit",
+						}}
+					/>
+				</chakra.a>
+			</chakra.button>
+			<chakra.button
+				border="2px solid red"
+				color="blackAlpha.800"
+				py="0.5"
+				px="1"
+				h="inherit"
+				_hover={{ bg: "blackAlpha.200", color: "blackAlpha.950" }}
+			>
+				<chakra.a>
+					<GithubSvgIcon
+						fill="currentColor"
+						style={{
+							maxHeight: "inherit",
+							width: "auto",
+
+							height: "inherit",
+						}}
+					/>
+				</chakra.a>
+				{/* <LinkedInSvgIcon w="150px" h="50px" /> */}
+			</chakra.button>
+		</HStack>
 	);
 }
 

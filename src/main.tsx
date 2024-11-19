74ec50f1a5232b53880147a0fdd64f5d7a3d3b27
gary-rivera <a.gary.rivera@gmail.com>
2024-11-18 23:42:10 -0800

    FINALLY

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import RuioWrapper from "ruio";
+// import RuioWrapper from "ruio";
 import { ProjectsProvider } from "./context/ProjectsContext";
 import { ChakraProvider } from "@chakra-ui/react";
-import { system } from "@/styles/theme.ts";
+import { system } from "@/styles/theme";
 
 import GlobalStyles from "@/styles/GlobalStyles";
 
-import App from "./App.tsx";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<RuioWrapper>
-			<ChakraProvider value={system}>
-				<GlobalStyles />
+		{/* <RuioWrapper> */}
+		<ChakraProvider value={system}>
+			<GlobalStyles />
 
-				<ProjectsProvider>
-					<App />
-				</ProjectsProvider>
-			</ChakraProvider>
-		</RuioWrapper>
+			<ProjectsProvider>
+				<App />
+			</ProjectsProvider>
+		</ChakraProvider>
+		{/* </RuioWrapper> */}
 	</StrictMode>,
 );

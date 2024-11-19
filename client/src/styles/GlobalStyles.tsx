1b4dc9c504176e9545e60d3868d77c5f3d2a77a0
gary-rivera <a.gary.rivera@gmail.com>
2024-11-18 17:26:16 -0800

    maybe? not sure if i still want the pattern tbh

--- client/src/styles/GlobalStyles.tsx
+++ client/src/styles/GlobalStyles.tsx
@@ -17,40 +17,53 @@ const GlobalStyles = () => {
 					backgroundColor: "#faedd0",
 					color: "black",
 					WebkitFontSmoothing: "antialiased",
-					MozOsxFontSmoothing: "grayscale",
-					"--primary-bg-color": "rgba(0, 0, 0, 0.035)",
+					// MozOsxFontSmoothing: "grayscale",
+					"--primary-bg-color": "rgba(0, 0, 0, 0.035)", // Static primary background color
 					"--primary-blue": "rgb(8, 145, 178)",
-					"--secondary-bg-color": "rgba(173, 216, 230, 1)",
-					"--bg-pattern": "url('https://www.toptal.com/designers/subtlepatterns/uploads/ep_naturalwhite.png')",
-					"--bg-filter": "opacity(20%) blur(0.01rem)",
+					"--secondary-bg-color": "rgba(173, 216, 230, 1)", // Static secondary color
+					"--bg-pattern": "url('https://www.toptal.com/designers/subtlepatterns/uploads/ep_naturalwhite.png')", // Static background pattern
+					backgroundImage: "var(--bg-pattern)",
+					"background-blend-mode": "multiply",
+					filter: "var(--bg-filter)",
+					zIndex: 0,
 				},
 				":root::before": {
+					backgroundColor: "#fffae1", // Static background color
+					// backgroundColor: "#fff8d5", // Static background color
+					// backgroundColor: "#FFFEF9", // Static background color
+					// backgroundColor: "#FFFDF7", // Static background color
+
+					"--bg-filter": "blur(0.05rem)", // Static filter
 					content: '""',
 					position: "absolute",
 					top: 0,
 					left: 0,
+					opacity: 0.55,
 					width: "100%",
 					height: "100%",
 					backgroundSize: "auto",
 					backgroundPosition: "center",
 					zIndex: -2,
-					filter: "var(--bg-filter)",
 					pointerEvents: "none",
+					// "background-blend-mode": "lighten",
 				},
 				":root::after": {
+					// backgroundColor: "#faedd0", // Static background color
+
 					content: '""',
 					position: "absolute",
 					top: 0,
 					left: 0,
 					width: "100%",
 					height: "100%",
-					opacity: 0.5,
-					zIndex: -1,
+					opacity: 0.2,
+					zIndex: 0,
 					backgroundImage:
-						"linear-gradient(to right, var(--secondary-bg-color) 1px, transparent 2px), linear-gradient(to bottom, var(--secondary-bg-color) 1px, transparent 2px)",
+						"linear-gradient(to right, rgb(8,145,178) 1px, transparent 2px), linear-gradient(to bottom, rgb(8,145,178) 1px, transparent 2px)",
 					filter: "blur(0.05rem)",
 					backgroundSize: "25px 25px",
 					pointerEvents: "none",
+					// mixBlendMode: "darken",
 				},
 			}}
 		/>

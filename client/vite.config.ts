b03784e74efe0a6e16d107df592f7eaa2ede2945
gary-rivera <a.gary.rivera@gmail.com>
2024-11-18 22:27:52 -0800

    update configs - hopefully builds to /dist dir

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,4 +6,9 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths(), svgr()],
+	build: {
+		outDir: "./dist", // Specifies the output directory
+		emptyOutDir: true, // Clears the output directory before each build
+		sourcemap: true, // Generates source maps for easier debugging
+	},
 });
